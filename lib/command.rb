dfab54ad04a877c8e414c59bedfb6b7e4a28c54b
Sean Ephraim <sean.ephraim@gmail.com>
2015-09-18 14:34:50 -0500

    Improve error handling when querying VCFs
    The --remove option is essential when querying with bcftools. However it
    will throw a cryptic error if you try to keep all fields. The pipeline
    will now try to re-execute an alternate query without removing any INFO
    tags, which should *hopefully* be successful every time.

--- lib/command.rb
+++ lib/command.rb
@@ -1,10 +1,19 @@
 # A library of available pipeline commands
 #
 # @author Sean Ephraim
+
+require 'logger'
+require 'open3'
+
 class Command
+  # Result file paths
   attr_reader :genes2regions_result
   attr_reader :regions2variants_result
   attr_reader :addgenes_result
+  attr_reader :addpredictions_result
+
+  @@log = Logger.new(STDERR)
+  @@log.level = Logger::INFO
 
   ##
   # Input columns:
@@ -42,9 +51,6 @@ class Command
 
   ##
   # Get a list of all variants within specified regions
-  #
-  # Input file format (tab-separated):
-  #   chr  pos_start  pos_end  gene
   ##
   def regions2variants(bed_file:, vcf_files:, out_file_prefix:, keep_tmp_files: false)
     tmp_vcfs = {}
@@ -54,6 +60,7 @@ class Command
 
       # Query all VCF files for variants
       vcf_files.each do |key, vcf|
+        next if key == 'dbnsfp' # DO NOT MERGE dbNSFP - ONLY ANNOTATE WITH IT
         tmp_source_vcf = "#{out_file_prefix}.#{vcf['source']}.tmp.vcf.gz"
         if vcf['fields'].nil?
           # Remove all INFO tags
@@ -64,30 +71,52 @@ class Command
         end
 
         # Query...
-        `bcftools annotate \
-           --remove '#{fields}' \
-           --regions-file '#{bed_file}' \
-           #{vcf['filename']} | \
-         bcftools filter \
-           --exclude 'ALT !~ "^[actgACGT,]*$"' \
-           --output #{tmp_source_vcf} \
-           --output-type z`
-        
-        # Index the results file...
-        `bcftools index --force --tbi #{tmp_source_vcf}`
+        @@log.info("Querying #{vcf['source']}...")
+        stdout, stderr = Open3.capture3(
+          "bcftools annotate \
+             --remove '#{fields}' \
+             --regions-file '#{bed_file}' \
+             --exclude 'TYPE=\"other\"' \
+             --output #{tmp_source_vcf} \
+             --output-type z \
+             #{vcf['filename']}"
+        )
 
-        # Store tmp file name (filename) and the original VCF that the data came from (parent)
-        tmp_vcfs[key] = {'filename' => tmp_source_vcf, 'parent' => vcf['filename']}
+        # Did bcftools return an error?
+        # Try again and don't remove any INFO tags this time
+        if !stderr.empty?
+          @@log.warn("bcftools returned an error for #{vcf['source']}. Trying another query method...")
+          stdout, stderr = Open3.capture3(
+            "bcftools annotate \
+               --regions-file '#{bed_file}' \
+               --exclude 'TYPE=\"other\"' \
+               --output #{tmp_source_vcf} \
+               --output-type z \
+               #{vcf['filename']}"
+          )
+        end
+
+        # Index the results file...
+        if !stderr.empty?
+          # ERROR
+          @@log.error("bcftools was not able to query #{vcf['source']}. Please check that file name and INFO tags are set correctly in your config file.")
+        else
+          # SUCCESS -- now create index file
+          @@log.info("Successfully queried #{vcf['source']}")
+          @@log.info("Creating index file for #{vcf['source']}...")
+          `bcftools index --force --tbi #{tmp_source_vcf}`
+
+          # Store tmp file name (filename) and the original VCF that the data came from (parent)
+          tmp_vcfs[key] = {'filename' => tmp_source_vcf, 'parent' => vcf['filename']}
+        end
       end
     end
 
     # Construct list of VCFs to merge
-    # NOTE: DO NOT MERGE dbNSFP - ONLY ANNOTATE WITH IT
     files_to_merge = []
     tmp_vcfs.each do |key, tmp_vcf|
-      if key != 'dbnsfp'
-        files_to_merge << tmp_vcf['filename']
-      end
+      next if key == 'dbnsfp' # DO NOT MERGE dbNSFP - ONLY ANNOTATE WITH IT
+      files_to_merge << tmp_vcf['filename']
     end
     files_to_merge << tmp_vcfs['dbsnp']['filename']
 
@@ -135,5 +164,46 @@ class Command
     File.unlink(header_file) if File.exist?(header_file)
     File.unlink("#{bed_file}.tmp.gz") if File.exist?("#{bed_file}.tmp.gz")
     File.unlink("#{bed_file}.tmp.gz.tbi") if File.exist?("#{bed_file}.tmp.gz.tbi")
+  end
+
+  ##
+  # Add predictions from dbNSFP
+  ##
+  def addpredictions(dbnsfp_file, vcf_file:,  out_file_prefix:)
+    # TODO Add dbNSFP predictions
+    @addpredictions_result = "#{out_file_prefix}.vcf.gz"
+    tmp_file = "#{out_file_prefix}.vcf"
+    `bcftools annotate \
+       --annotations #{dbnsfp_file['filename']} \
+       --columns #{dbnsfp_file['fields'].map { |f| "INFO/#{f}" }.join(',')}
+       --output #{@addpredictions_result} \
+       --output-type v \
+       #{vcf_file}`
+#       .each_line do |vcf_row|
+#         if vcf_row.match(/^#/)
+#           # TODO Print header
+#         else
+#           total_num_preds = 0
+#           num_path_preds = 0
+#           dbnsfp_file['fields'].each do |field|
+#             # Skip field if: 
+#             # 1. It's not a prediction
+#             # 2. The prediction is unknown
+#             next if !field.match(/_PRED$/i)
+#             next if preds.all? {|pred| pred == '.' || pred == 'U'}
+#
+#             preds = vcf_row.match(/[\t;]#{field}=([^;\t]*)/)[1].split(';')
+#             if field.match(/^SIFT_PRED$/) && preds.include('D')
+#               # SIFT prediction
+#             elsif field.match(/^POLYPHEN2_PRED$/)
+#               # TODO Analyze all other predictions too
+#             end
+#           end
+#         end
+#       end
+
+    # TODO Add *nominal* predictions for convervation scores (GERP++ and phyloP)
+    # TODO Add final prediction
+
   end
 end
--- pipeline.rb
+++ pipeline.rb
@@ -5,16 +5,29 @@ require_relative File.join('lib', 'boot.rb')
 F_IN = ARGV[0]
 FILE_PREFIX = F_IN.sub(/#{File.extname(F_IN)}/, '')
 
-comm = Command.new
+cmd = Command.new
 
 # Convert gene names to gene regions
-comm.genes2regions(genes_file: F_IN, ref_file: CONFIG['gene_regions_ref_file'], out_file_prefix: FILE_PREFIX)
+cmd.genes2regions(genes_file: F_IN,
+                  ref_file: CONFIG['gene_regions_ref_file'],
+                  out_file_prefix: FILE_PREFIX)
 
-# TODO Get variants from gene regions
-comm.regions2variants(bed_file: comm.genes2regions_result, vcf_files: CONFIG['annotation_files'], out_file_prefix: FILE_PREFIX)
+# Get variants from gene regions
+cmd.regions2variants(bed_file: cmd.genes2regions_result,
+                     vcf_files: CONFIG['annotation_files'],
+                     out_file_prefix: FILE_PREFIX)
 
 # Add gene symbol to each record in VCF
-comm.addgenes(bed_file: comm.genes2regions_result, vcf_file: comm.regions2variants_result, out_file_prefix: FILE_PREFIX)
-                   
-# TODO Add expert-curated variants that are
-#      missing in the original list of variants
+cmd.addgenes(bed_file: cmd.genes2regions_result,
+             vcf_file: cmd.regions2variants_result,
+             out_file_prefix: FILE_PREFIX)
+
+## TODO Add expert-curated variants that are
+##      missing in the original list of variants
+#cmd.addvariants(vcf_file: CONFIG['annotation_files']['expert_curations'],
+#                 out_file_prefix: FILE_PREFIX)
+
+# TODO Annotate with dbNSFP
+#puts "Annotating with dbNSFP..."
+#cmd.addpredictions(dbnsfp_file: CONFIG['annotation_files']['dbnsfp'],
+#                   out_file_prefix: FILE_PREFIX)
