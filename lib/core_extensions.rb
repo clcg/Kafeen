e294c3003bca0215513d5119feb82ef44a2475e0
Sean Ephraim <sean.ephraim@gmail.com>
2016-06-24 09:27:16 -0500

    Add method for producing blue text

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -55,40 +55,14 @@ module CoreExtensions
       def yellow
         colorize(33)
       end
-    end
-  end
-end
 
-module CoreExtensions
-  module String
-    module Colorize
-      # colorization
-      def colorize(color_code)
-        "\e[#{color_code}m#{self}\e[0m"
-      end
-    
-      def red
-        colorize(31)
-      end
-    
-      def green
-        colorize(32)
-      end
-    
-      def yellow
-        colorize(33)
-      end
-    
+      ##
+      # Blue
+      #
+      # Print blue text to stdout.
+      ##
       def blue
         colorize(34)
-      end
-    
-      def pink
-        colorize(35)
-      end
-    
-      def light_blue
-        colorize(36)
       end
     end
   end
